c5c42eccea5c602c24c256cf356f13642319f7cb
charlie1099 <cdcharlesworth@gmail.com>
2021-11-20 12:43:38 -0700

    added comments

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,15 @@
 // Sequelize constructor from the librery
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-require("dotenv").config()
+require('dotenv').config();
 
-// create connection to the database, psaa in the info for MySQL info
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306
-})
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
-module.exports = sequelize
+module.exports = sequelize;
